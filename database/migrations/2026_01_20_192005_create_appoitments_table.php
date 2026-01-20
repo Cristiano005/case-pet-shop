41f14eadba936fa2d63b339be2bcf74fed29566f
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-01-20 16:34:18 -0300

    feat: appoitments migrate added

---
+++ database/migrations/2026_01_20_192005_create_appoitments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appoitments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable(false);
+            $table->unsignedInteger('animal_id')->nullable(false);
+            $table->dateTime('appointment_date')->nullable(false);
+            $table->string('status', 100)->nullable(false)->default('scheduled');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('animal_id')->references('id')->on('animals')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appoitments');
+    }
+};
