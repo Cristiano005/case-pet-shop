968a4a12b1f2df72b1afc6cb7f90a18101f40845
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-01-22 17:32:32 -0300

    feat: add React Router and pages structure

---
+++ front-end/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router";
+
+function Home() {
+  return (
+    <>
+      <NavLink to="/signup">All Concerts</NavLink>
+    </>
+  )
+}
+
+export default Home
---
+++ front-end/src/pages/SignUp/index.jsx
@@ -0,0 +1,9 @@
+function SignUp() {
+
+    return (
+        <h2> Faça seu Sign Up </h2>
+    )
+
+}
+
+export default SignUp;
