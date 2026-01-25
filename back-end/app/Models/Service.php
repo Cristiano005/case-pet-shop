b2b9f04c9cf23ecb75be88d82748bda950fe21e8
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-01-25 15:29:07 -0300

    feat: new services table added

--- back-end/app/Models/Service.php
+++ back-end/app/Models/Service.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    protected $fillable = ['name', 'description', 'price'];
+
+    use HasFactory;
 }
