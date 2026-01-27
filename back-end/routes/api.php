bb5d8aac6ebbd8035be52d37b175008c41a84e1a
Cristiano005 <cristianosilvadesousa05@gmail.com>
2026-01-27 13:15:49 -0300

    refactor: add route group to api routes

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\Http\Controllers\Api\V1\
-    { AnimalController, AppointmentController, UserController, ServiceController, FreeScheduleController };
+use App\Http\Controllers\Api\V1\{AnimalController, AppointmentController, UserController, ServiceController, FreeScheduleController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/free-schedules', [FreeScheduleController::class, 'index']);
-Route::get('/animals/user/{user:id}', [AnimalController::class, 'getAnimalsById']);
 Route::get('/services', [ServiceController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
-Route::post('/animals', [AnimalController::class, 'store']);
 Route::post('/appointments', [AppointmentController::class, 'store']);
+
+Route::controller(AnimalController::class)->group(function () {
+    Route::get('/animals/user/{user:id}', [AnimalController::class, 'getAnimalsById']);
+    Route::post('/animals', [AnimalController::class, 'store']);
+});
